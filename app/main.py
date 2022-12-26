2ed9a598544aec29c079b81b2f9e1755adb19c29
Madeeha <maadeeeehaa@gmail.com>
2022-12-26 11:11:00 -0700

    alembic install and cleanup

--- app/main.py
+++ app/main.py
@@ -2,7 +2,23 @@ from fastapi import FastAPI, Depends
 from db import engine
 import models
 from routers import auth, todos, users
-from company import companyapis, dependencies
+
+print(
+    "\x1b[6;30;42m"
+    + "Congratulations the backend is running on http://localhost:8000"
+    + "\x1b[0m"
+)
+print(
+    "\x1b[6;30;42m"
+    + "Congratulations the API docs are running on http://localhost:8000/docs"
+    + "\x1b[0m"
+)
+print(
+    "\x1b[6;30;42m"
+    + "Congratulations pgAdmin is running on http://localhost:8080"
+    + "\x1b[0m"
+)
+
 
 app = FastAPI()
 
@@ -13,10 +29,3 @@ models.Base.metadata.create_all(bind=engine)
 app.include_router(auth.router)
 app.include_router(todos.router)
 app.include_router(users.router)
-app.include_router(
-    companyapis.router,
-    prefix="/companyapis",
-    tags=["companyapis"],
-    dependencies=[Depends(dependencies.get_token_header)],
-    responses={418: {"418": "Internal Use Only"}},
-)
